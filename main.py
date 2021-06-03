d71250e9159698efcb1d359e5f2b837ec8e581a3
FreakingJackpot <70316335+FreakingJackpot@users.noreply.github.com>
2021-06-03 12:46:18 +0400

    dfd

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import requests
+from collections import Counter
 
 
 def get_vacancies():
     count = get_vacancies_count()
     dict = {}
-    for i in range(1):
+    for i in range(2):
         params = {
             'text': 'IT',
             'area': 113,
-            'page': 0,
+            'page': i,
             'per_page': 100,
         }
 
-        data = requests.get('https://api.hh.ru/vacancies', params=params)
-        data = data.json()
+        data = requests.get('https://api.hh.ru/vacancies', params=params).json()
 
         for i in data['items']:
-            if  i['name'] in dict:
-                dict[i['name']] = get_key_skills(i['url'])+dict[i['name']]
+            if i['name'] in dict:
+                tmp = get_key_skills(i['url'])
+                counter = dict[i['name']]['counter']
+                for word in tmp:
+                    counter[word] += 1
+                dict[i['name']] = {'key_skills': list(set(get_key_skills(i['url']) + dict[i['name']]['key_skills'])),
+                                   'counter': counter}
             else:
-                dict[i['name']] = get_key_skills(i['url'])
-
+                tmp = get_key_skills(i['url'])
+                counter = Counter()
+                for word in tmp:
+                    counter[word] += 1
+                dict[i['name']] = {'key_skills': get_key_skills(i['url']), 'counter': counter}
+            print(len(dict))
+        print(dict)
     return dict
 
 
@@ -29,11 +39,10 @@ def get_vacancies_count():
         'text': 'IT',
         'area': 113,
         'page': 0,
-        'per_page': 100,
+        'per_page': 1,
     }
 
-    data = requests.get('https://api.hh.ru/vacancies', params=params)
-    data = data.json()
+    data = requests.get('https://api.hh.ru/vacancies', params=params).json()
     return data['found']
 
 
@@ -43,12 +52,14 @@ def get_key_skills(url):
     return [i['name'] for i in data['key_skills']]
 
 
-params = {
-    'text': 'IT',
-    'area': 113,
-    'page': 0,
-    'per_page': 100,
-}
+data = get_vacancies()
 
-print(get_vacancies())
-
+"""
+with open('vacancies.txt', 'w') as f:
+    for i in data.keys():
+        str = ''
+        c = dict(data[i]['counter'])
+        for j in c.keys():
+            str += '{} : {}\t'.format(j, c[j])
+        f.write(i + ': ' + str + '\n')
+"""
